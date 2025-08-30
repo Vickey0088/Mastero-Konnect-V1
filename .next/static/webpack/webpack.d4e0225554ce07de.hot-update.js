cf3de8ed21a6a8d29a5329e3c81f3631c1228dae
thohiraa <thohi1210@gmail.com>
2025-08-30 17:25:25 +0530

    Bug fixes

--- .next/static/webpack/webpack.d4e0225554ce07de.hot-update.js
+++ .next/static/webpack/webpack.d4e0225554ce07de.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "8fe63f1edb37d36b"; }
+/******/ 	__webpack_require__.h = function() { return "e76b04b53b1aa133"; }
 /******/ }();
 /******/ 
 /******/ }
